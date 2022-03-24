26f16dd6525c24031aaf642f9ef450ae87d58942
reynaldo29 <acm19@outlook.com>
2022-03-24 17:35:00 -0500

    Segundo archivo

---
+++ StudentModel.java
@@ -0,0 +1,12 @@
+public class Trabajador {
+    public String nombre;
+    public String apellido;
+
+    public static void main( String[] args ) {
+
+    	public int a = 15, b = 10, c = 0;
+    	c = a + b;
+    	System. out. println( "La suma de a y b es: " + c );
+	}
+ 
+  }
